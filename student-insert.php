c51224b0a14c4c0701669780ba8f98d3a36a5145
vidura <vidurada@gmail.com>
2017-11-27 20:22:24 +0530

    stud-int func

--- student-insert.php
+++ student-insert.php
@@ -185,213 +185,135 @@
         </nav>
 
 
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card">
+				<div class="content">
+						<div class="container-fluid">
+								<div class="row">
+										<div class="col-md-8">
+												<div class="card">
+														<div class="header">
+																<h4 class="title">Edit Profile</h4>
+														</div>
+														<div class="content">
+																<form>
+																		<div class="row">
+																				<div class="col-md-5">
 
-                            <div class="header">
-                                <h4 class="title">Students</h4>
-                                <p class="category">Education Level</p>
-                            </div>
-                            <div class="content">
-                                <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
+																				</div>
+																		</div>
 
-                                <div class="footer">
-                                    <div class="legend">
-                                        <i class="fa fa-circle text-info"></i> Undergraduate
-                                        <i class="fa fa-circle text-danger"></i> Postgraduate
-                                        <!--<i class="fa fa-circle text-warning"></i> Unsubscribe-->
-                                    </div>
-                                    <hr>
-                                    <div class="stats">
-                                        <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+																		<div class="row">
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>First Name</label>
+																								<input type="text" class="form-control" placeholder="Company" value="Mike">
+																						</div>
+																				</div>
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>Last Name</label>
+																								<input type="text" class="form-control" placeholder="Last Name" value="Andrew">
+																						</div>
+																				</div>
+																		</div>
 
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="header">
-                                <h4 class="title">Student GPA Chart</h4>
-                                <p class="category">24 Hours performance</p>
-                            </div>
-                            <div class="content">
-                                <div id="chartHours" class="ct-chart"></div>
-                                <div class="footer">
-                                    <div class="legend">
-                                        <i class="fa fa-circle text-info"></i> Undergraduate
-                                        <i class="fa fa-circle text-danger"></i> Postgraduate
-                                        <!--<i class="fa fa-circle text-warning"></i> Click Second Time-->s
-                                    </div>
-                                    <hr>
-                                    <div class="stats">
-                                        <i class="fa fa-history"></i> Upto Final Semester
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+																		<div class="row">
+																				<div class="col-md-12">
+																						<div class="form-group">
+																								<label>Address</label>
+																								<input type="text" class="form-control" placeholder="Home Address" value="">
+																						</div>
+																				</div>
+																		</div>
+																		<div class="row">
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>Age</label>
+																								<input type="text" class="form-control" placeholder="Age" value="">
+																						</div>
+																				</div>
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>DOB</label>
+																								<input type="text" class="form-control" placeholder="Academic Year" value="">
+																						</div>
+																				</div>
 
+																		</div>
 
+																		<div class="row">
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>City</label>
+																								<input type="text" class="form-control" placeholder="City" value="">
+																						</div>
+																				</div>
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>University</label>
+																								<input type="text" class="form-control" placeholder="University" value="">
+																						</div>
+																				</div>
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="card ">
-                            <div class="header">
-                                <h4 class="title">2014 Sales</h4>
-                                <p class="category">All products including Taxes</p>
-                            </div>
-                            <div class="content">
-                                <div id="chartActivity" class="ct-chart"></div>
+																		</div>
+																		<div class="row">
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>Degree</label>
+																								<input type="text" class="form-control" placeholder="degree" value="">
+																						</div>
+																				</div>
+																				<div class="col-md-6">
+																						<div class="form-group">
+																								<label>Academic Year</label>
+																								<input type="text" class="form-control" placeholder="Academic Year" value="">
+																						</div>
+																				</div>
 
-                                <div class="footer">
-                                    <div class="legend">
-                                        <i class="fa fa-circle text-info"></i> Tesla Model S
-                                        <i class="fa fa-circle text-danger"></i> BMW 5 Series
-                                    </div>
-                                    <hr>
-                                    <div class="stats">
-                                        <i class="fa fa-check"></i> Data information certified
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+																		</div>
 
-                    <div class="col-md-6">
-                        <div class="card ">
-                            <div class="header">
-                                <h4 class="title">Tasks</h4>
-                                <p class="category">Backend development</p>
-                            </div>
-                            <div class="content">
-                                <div class="table-full-width">
-                                    <table class="table">
-                                        <tbody>
-                                            <tr>
-                                                <td>
-													<div class="checkbox">
-						  							  	<input id="checkbox1" type="checkbox">
-						  							  	<label for="checkbox1"></label>
-					  						  		</div>
-                                                </td>
-                                                <td>Sign contract for "What are conference organizers afraid of?"</td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
-                                                        <i class="fa fa-edit"></i>
-                                                    </button>
-                                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-													<div class="checkbox">
-						  							  	<input id="checkbox2" type="checkbox" checked>
-						  							  	<label for="checkbox2"></label>
-					  						  		</div>
-                                                </td>
-                                                <td>Lines From Great Russian Literature? Or E-mails From My Boss?</td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
-                                                        <i class="fa fa-edit"></i>
-                                                    </button>
-                                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-													<div class="checkbox">
-						  							  	<input id="checkbox3" type="checkbox">
-						  							  	<label for="checkbox3"></label>
-					  						  		</div>
-                                                </td>
-                                                <td>Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-												</td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
-                                                        <i class="fa fa-edit"></i>
-                                                    </button>
-                                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-													<div class="checkbox">
-						  							  	<input id="checkbox4" type="checkbox" checked>
-						  							  	<label for="checkbox4"></label>
-					  						  		</div>
-                                                </td>
-                                                <td>Create 4 Invisible User Experiences you Never Knew About</td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
-                                                        <i class="fa fa-edit"></i>
-                                                    </button>
-                                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-													<div class="checkbox">
-						  							  	<input id="checkbox5" type="checkbox">
-						  							  	<label for="checkbox5"></label>
-					  						  		</div>
-                                                </td>
-                                                <td>Read "Following makes Medium better"</td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
-                                                        <i class="fa fa-edit"></i>
-                                                    </button>
-                                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-													<div class="checkbox">
-						  							  	<input id="checkbox6" type="checkbox" checked>
-						  							  	<label for="checkbox6"></label>
-					  						  		</div>
-                                                </td>
-                                                <td>Unfollow 5 enemies from twitter</td>
-                                                <td class="td-actions text-right">
-                                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
-                                                        <i class="fa fa-edit"></i>
-                                                    </button>
-                                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+																		<div class="row">
+																				<div class="col-md-12">
+																						<div class="form-group">
+																								<label>About Me</label>
+																								<textarea rows="5" class="form-control" placeholder="Here can be your description" value="Mike">Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</textarea>
+																						</div>
+																				</div>
+																		</div>
 
-                                <div class="footer">
-                                    <hr>
-                                    <div class="stats">
-                                        <i class="fa fa-history"></i> Updated 3 minutes ago
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+																		<button type="submit" class="btn btn-info btn-fill pull-right">Update Profile</button>
+																		<div class="clearfix"></div>
+																</form>
+														</div>
+												</div>
+										</div>
+										<div class="col-md-4">
+												<div class="card card-user">
+														<div class="image">
+																<img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+														</div>
+														<div class="content">
+																<div class="author">
+																		 <a href="#">
+																		<img class="avatar border-gray" src="assets/img/faces/face-3.jpg" alt="..."/>
+
+																			<h4 class="title">Mike Andrew<br />
+																				 <small>michael24</small>
+																			</h4>
+																		</a>
+																</div>
+																<p class="description text-center"> "Lamborghini Mercy <br>
+																										Your chick she so thirsty <br>
+																										I'm in that two seat Lambo"
+																</p>
+														</div>
+														<hr>
+
+												</div>
+										</div>
+
+								</div>
+						</div>
+				</div>
 
 
         <footer class="footer">
